2dfa5aa5bce6fdae657319744f570c4d397a7751
ytyaru <ytyaru@outlook.jp>
2020-10-10 16:37:23 +0900

    user func

---
+++ src/0_guide/25_user_func.bas
@@ -0,0 +1,3 @@
+Function VOL(a, b, c)
+	VOL = a * b * c
+End Function
---
+++ src/0_guide/25_user_func.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# LibreOffice Calc ユーザ定義関数のPython版
+def VOL_PY(a, b, c):
+    return a * b * c
